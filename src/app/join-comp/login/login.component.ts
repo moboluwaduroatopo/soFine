8474836c7934ec076810fa135efde9dececcaf46
aylalah <ayoadelala@yahoo.com>
2018-08-29 11:28:09 +0100

    material test join page cleared for sam update

--- src/app/join-comp/login/login.component.ts
+++ src/app/join-comp/login/login.component.ts
@@ -9,13 +9,6 @@ import {FormControl, Validators} from '@angular/forms';
 
 export class LoginComponent implements OnInit {
 
-email = new FormControl('', [Validators.required, Validators.email]);
- getErrorMessage() {
-    return this.email.hasError('required') ? 'You must enter a value' :
-        this.email.hasError('email') ? 'Not a valid email' :
-            '';
-  };
-
   constructor() { }
 
   ngOnInit() {
