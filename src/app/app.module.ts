0c066e62251722af322d780418da2355a1195a4d
Olarinde <delightolarinde@gmail.com>
2018-08-29 09:45:03 +0100

    created tailor-home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { SupportComponent } from './guest-nav/support/support.component';
 import { PrivacyComponent } from './guest-nav/privacy/privacy.component';
 import { GuestDashComponent } from './guest-nav/guest-dash/guest-dash.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { TailorHomeComponent } from './homepages/tailor-view/tailor-home/tailor-home.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { AppRoutingModule } from './/app-routing.module';
     AboutComponent,
     SupportComponent,
     PrivacyComponent,
-    GuestDashComponent
+    GuestDashComponent,
+    TailorHomeComponent
   ],
   imports: [
     BrowserModule,
