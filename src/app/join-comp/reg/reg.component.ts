2c11d06e28824df51ad91f5c16dc52e7738a4995
sammykrin <olamilehinsamuelolamide@gmail.com>
2018-08-30 11:53:17 -0700

    registration ready

--- src/app/join-comp/reg/reg.component.ts
+++ src/app/join-comp/reg/reg.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-reg',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reg.component.css']
 })
 export class RegComponent implements OnInit {
-
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+  isEditable = false;
   constructor() { }
 
   ngOnInit() {
+    
   }
 
 }
